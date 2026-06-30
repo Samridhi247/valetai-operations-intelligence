1242482e1e2e8f3bd6d60f257096ab5489a7a6d7
Sinha <samridhi.sinha@accenture.com>
2026-07-01 01:32:49 +0530

    rec set to 15

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def ask_question(request: QuestionRequest):
     result = graph_app.invoke(
         {"question": request.question},
         config={
-            "recursion_limit": 10
+            "recursion_limit": 15
             # "recursion_limit": 4
         }
     )
